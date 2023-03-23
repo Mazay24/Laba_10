a99b609c353267d0f8cc29b306f9b489c02f0fad
Исаев Никита <96546414+Mazay24@users.noreply.github.com>
2023-03-23 16:23:59 +0300

    Add files via upload

---
+++ Лаба10/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Лаба10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Garage garage = new Garage(new Car("Toyota"), new Car("Tesla"));
+            Washer washer = new Washer();
+
+            CarWasher carWasher = washer.Wash;
+
+            foreach (Car car in garage.cars)
+            {
+                carWasher(car);
+            }
+        }
+
+      
+}
+    }
+delegate void CarWasher(Car car);
+    
+    class Car
+    {
+        public string Name { get; set; }
+
+        public Car(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Garage
+    {
+        public List<Car> cars = new List<Car>();
+
+        public Garage(params Car[] autos)
+        {
+            foreach (Car car in autos)
+            {
+                cars.Add(car);
+            }
+        }
+    }
+
+    class Washer
+    {
+        public void Wash(Car car)
+        {
+            Console.WriteLine($"Автомобиль {car.Name} помыт.");
+        }
+    }
+
+
